acee6b12f1197286f657f736ac96bd9184a55cbd
domagala2021 <domagala2021@DESKTOP-8VVMPG5>
2024-08-05 16:34:53 +0100

    built-in special variables

--- 01-passing_arguments.sh
+++ 01-passing_arguments.sh
@@ -4,3 +4,6 @@ echo "do skryptu $0 przekazano:"
 echo "argument pierwszy: $1"
 echo "argument drugi: $2"
 echo "argument trzeci: $3"
+
+echo "Argumenty przekazane do skryptu: $@"
+echo "Liczba przekazanych argumentów: $#"
