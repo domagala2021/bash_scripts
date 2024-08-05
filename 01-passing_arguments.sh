4370811157d587d16d5ad0054a82840f726780c2
domagala2021 <domagala2021@DESKTOP-8VVMPG5>
2024-08-05 17:22:54 +0100

    Korekta zapisu na wyjściu

--- 01-passing_arguments.sh
+++ 01-passing_arguments.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "do skryptu $0 przekazano:"
-echo "argument pierwszy: $1"
-echo "argument drugi: $2"
-echo "argument trzeci: $3"
+echo "Do skryptu $0 przekazano:"
+echo "argument nr 1: $1"
+echo "argument nr 2: $2"
+echo "argument nr 3: $3"
 
-echo "Argumenty przekazane do skryptu: $@"
+echo -e
 echo "Liczba przekazanych argumentów: $#"
 
 # Iteracja argumentów przekazywanych do skryptu - for loop
