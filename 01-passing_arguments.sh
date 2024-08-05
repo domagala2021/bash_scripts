cdd21688c8d8ee24409ed8c016a89618b04eccf0
domagala2021 <domagala2021@DESKTOP-8VVMPG5>
2024-08-05 17:18:43 +0100

    iteracja argumentów przekazywanych do skryptu - pętla for

--- 01-passing_arguments.sh
+++ 01-passing_arguments.sh
@@ -7,3 +7,20 @@ echo "argument trzeci: $3"
 
 echo "Argumenty przekazane do skryptu: $@"
 echo "Liczba przekazanych argumentów: $#"
+
+# Iteracja argumentów przekazywanych do skryptu - for loop
+
+echo -e
+
+if [ $# -eq 0 ]; then
+    echo "Nie podano żadnych argumentów"
+    exit 1
+fi
+
+lp=1
+
+for arg in $@
+do
+    echo "argument nr $lp:" $arg
+    lp=$((lp + 1))
+done
