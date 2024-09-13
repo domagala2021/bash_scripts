13d2af9600354c84ff206eef00be24a5768e1bc6
domagala2021 <domagala.praca@gmail.com>
2024-09-13 22:11:13 +0100

    rozwiąznie problemu ścieżki do katalogu bieżącego

---
+++ 06-for_loops-2a.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+current_dir=$1
+backup_dir="$current_dir/backup"
+prefix=$(date +"%Y%m%d_")
+
+if [ ! -d "$backup_dir" ]; then
+    mkdir $backup_dir
+fi
+
+for file in $(ls $current_dir)
+do
+    cp "$current_dir/$file" "$backup_dir/$prefix$file"
+    # echo "$prefix$file"
