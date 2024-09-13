7b7a924b6c77d04765e05254158cf900e22a4d48
domagala2021 <domagala.praca@gmail.com>
2024-09-13 11:30:05 +0100

    Poprawka skryptu po nieudanej próbie wywołania w cronie

--- 06-for_loops.sh
+++ 06-for_loops.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-
-backup_dir="backup"
+current_dir="/home/domagala2021/bash_scripts"
+backup_dir="$current_dir/backup"
 prefix=$(date +"%Y%m%d_")
 
 if [ ! -d "$backup_dir" ]; then
     mkdir $backup_dir
 fi
 
-    for file in *.*
+for file in $(ls $current_dir)
 do
-    cp $file "$backup_dir/$prefix$file"
+    cp "$current_dir/$file" "$backup_dir/$prefix$file"
     # echo "$prefix$file"
 
 done
